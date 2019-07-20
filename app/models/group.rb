fae131b040753b82f937b06657eaae6bfc1f86a5
kanegon10 <buzzsystem1@ezweb.ne.jp>
2019-07-20 18:34:25 +0900

    update relation
    update : 機能修正
    
    group_userとuserとgroupのリレーションでの
    foreign_keyを追記して外部キーを指定しました。

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ApplicationRecord
-  has_many :group_users
+  has_many :group_users,foreign_key: :group_id
   has_many :users,through: :group_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
- has_many :group_users
+ has_many :group_users,foreign_key: :user_id
  has_many :groups,through: :group_users
+ 
 end
