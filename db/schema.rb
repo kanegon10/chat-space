a9f285ff128d4a4f2f0e5d0dda9c09a02d135f06
kanegon10 <buzzsystem1@ezweb.ne.jp>
2019-07-20 17:26:19 +0900

    Table creation failure
    revert : 変更取り消し
    
    group_users テーブルを作成しようとして　user_idカラムの型を　integers と間違えて打ってmigrateしてしまったので
    rails db:rollback　を実行してmigrateを取り消したら
    messages テーブルが消えていまいました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190720075705) do
+ActiveRecord::Schema.define(version: 20190720074624) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "body",       limit: 65535
-    t.string   "image"
-    t.integer  "group_id"
-    t.integer  "user_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
