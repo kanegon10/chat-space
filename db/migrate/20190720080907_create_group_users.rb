643627791a16de878886f69f5889c3f3c2022a87
kanegon10 <buzzsystem1@ezweb.ne.jp>
2019-07-20 17:36:03 +0900

    create messages class & group_users class
    add : 新規作成
    
    create_group_users.rb　の
    
    t.integers :user_id　を
    t.integer :user_id に書き直して
    rake db:migrateを実行してmesseageテーブルとgroup_userテーブルを作成しました。

--- db/migrate/20190720080907_create_group_users.rb
+++ db/migrate/20190720080907_create_group_users.rb
@@ -2,7 +2,7 @@ class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
       t.integer :group_id
-      t.integers :user_id
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190720074624) do
+ActiveRecord::Schema.define(version: 20190720080907) do
+
+  create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "body",       limit: 65535
+    t.string   "image"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
