009935822be7505a7dde8b18f49529fc89542f70
kanegon10 <buzzsystem1@ezweb.ne.jp>
2019-07-21 13:24:28 +0900

    update user & group models
    update : 機能追加
    userとgroupのモデルファイルに
    validates :name, presence: true　を追記しました。

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
   has_many :group_users,foreign_key: :group_id
   has_many :users,through: :group_users
   has_many :messages,foreign_key: :group_id
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
  has_many :group_users,foreign_key: :user_id
  has_many :groups,through: :group_users
  has_many :messages,foreign_key: :user_id
- end
+ validates :name, presence: true
+end
