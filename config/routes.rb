01e9808cb6048e3a49367bd6d454ca04b54b3044
kanegon10 <buzzsystem1@ezweb.ne.jp>
2019-08-15 12:27:32 +0900

    Remove:必要ないコードを削除しました。
    index.html.haml　の　%body　から上の記述を
    削除しました。
    
    routes.rb の　root to: 'messages#index' 以外の
    pathを削除しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
   root to: 'messages#index'
 
-  get 'messages/show'
-
-  get 'group_users/index'
-
-  get 'group_users/show'
-
-  get 'group/index'
-
-  get 'group/show'
-
-  get 'user/index'
-
-  get 'user/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
